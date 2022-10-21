eb2ff2f7fa0a3eea402850b2f204be90feaa7fb6
Rangel8uu <rangel8u@gmail.com>
2022-10-21 17:22:04 -0500

    feat:Vista de los rubros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,21 +10,21 @@ const routes: Routes = [
     loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule),
     //canLoad: [AuthGuard]
   },
-  {
-    path: 'admin',
-    loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
+  //{
+    //path: 'user',
+    //loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule),
     //canLoad: [AuthGuard]
-  },
-  {
-    path: 'reedevento',
-    loadChildren: () => import('./modules/reedevento/reedevento.module').then(m => m.ReedEventoModule),
+  //},
+  //{
+    //path: 'reedevento',
+    //loadChildren: () => import('./modules/reedevento/reedevento.module').then(m => m.ReedEventoModule),
     //canLoad: [AuthGuard]
-  },
-  {
-    path: 'reedeventoadmin',
-    loadChildren: () => import('./modules/reedeventoadmin/reedeventoadmin.module').then(m => m.ReedEventoAdminModule),
+  //},
+  //{
+    //path: 'reedeventoadmin',
+    //loadChildren: () => import('./modules/reedeventoadmin/reedeventoadmin.module').then(m => m.ReedEventoAdminModule),
     //canLoad: [AuthGuard]
-  },
+  //},
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'portal' }
 ];
